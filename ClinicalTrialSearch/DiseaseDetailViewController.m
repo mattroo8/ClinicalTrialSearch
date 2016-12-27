b9ba9333c0a9d99dc78ff9a8c36b955e80ccbd43
matt rooney <mattrooney@matts-MacBook-Pro.local>
2016-12-27 10:31:22 +0000

    Second commit

---
+++ ClinicalTrialSearch/DiseaseDetailViewController.m
@@ -0,0 +1,27 @@
+//
+//  DiseaseDetailViewController.m
+//  ClinicalTrialSearch
+//
+//  Created by matt rooney on 26/12/2016.
+//  Copyright © 2016 matt rooney. All rights reserved.
+//
+
+#import "DiseaseDetailViewController.h"
+
+@interface DiseaseDetailViewController ()
+
+@end
+
+@implementation DiseaseDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ClinicalTrialSearch/MedicalTrialDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  MedicalTrialDetailViewController.m
+//  ClinicalTrialSearch
+//
+//  Created by matt rooney on 26/12/2016.
+//  Copyright © 2016 matt rooney. All rights reserved.
+//
+
+#import "MedicalTrialDetailViewController.h"
+
+@interface MedicalTrialDetailViewController ()
+
+@end
+
+@implementation MedicalTrialDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
